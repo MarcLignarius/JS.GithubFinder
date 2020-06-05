40d5b7407418ad0de4cb0ed0056c3fd0251dc21a
Marc Davies <marcdaviesriot@gmail.com>
2020-06-04 22:41:05 -0700

    display profile and mobile styles

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
-// Init
+// Init Github
 const github = new Github();
+// Init UI
+const ui = new UI();
 
 // Search input
 const searchUser = document.getElementById('searchUser');
@@ -15,6 +17,7 @@ searchUser.addEventListener('keyup', (e) => {
         // Show alert
       } else {
         // Show profile
+        ui.showProfile(data.profile);
       }
     });
   } else {
